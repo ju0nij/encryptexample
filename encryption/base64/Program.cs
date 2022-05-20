0aa0c6695c17ad31e4f39b84a43755e3f85a5cfe
Juyeong J <juyeong.j@outlook.com>
2022-05-21 02:14:59 +0900

    1
    - Base64 example
    - ProtectedData example

---
+++ encryption/base64/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace base64
+{
+    class Base64
+    {
+        public string EncodeBase64(string text)
+        {
+            byte[] Byte = Encoding.UTF8.GetBytes(text);
+            var data = System.Convert.ToBase64String(Byte);
+            return data;
+        }
+        public string DecodeBase64(string text)
+        {
+            byte[] Byte = System.Convert.FromBase64String(text);
+            var data = Encoding.Default.GetString(Byte);
+            return data;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("변환할 텍스트를 입력해주세요: ");
+            Base64 base64 = new Base64();
+            string text = Console.ReadLine();
+            string encode = base64.EncodeBase64(text);
+            Console.WriteLine("Base64 Encode: " + encode);
+            string decode = base64.DecodeBase64(encode);
+            Console.WriteLine("Base64 Decode: " + decode);
+        }
+    }
+}
---
+++ encryption/protecteddata/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protecteddata
+{
+    //https://docs.microsoft.com/ko-kr/dotnet/api/system.security.cryptography.protecteddata
+
+    class ProtectedDataEx
+    {
+        public string Protect(string text)
+        {
+            var PasswordProtect = Convert.ToBase64String(ProtectedData.Protect(Encoding.UTF8.GetBytes(text), null, DataProtectionScope.LocalMachine));
+            return PasswordProtect;
+        }
+        public string Unprotect(string text)
+        {
+            var PasswordUnprotect = Encoding.UTF8.GetString(ProtectedData.Unprotect(Convert.FromBase64String(text), null, DataProtectionScope.LocalMachine));
+            return PasswordUnprotect;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("변환할 텍스트를 입력해주세요: ");
+            ProtectedDataEx protecteddata = new ProtectedDataEx();
+            string text = Console.ReadLine();
+            string encode = protecteddata.Protect(text);
+            Console.WriteLine("Data Encode: " + encode);
+            string decode = protecteddata.Unprotect(encode);
+            Console.WriteLine("Data Decode: " + decode);
+        }
+    }
+}
